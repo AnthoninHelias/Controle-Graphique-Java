4a6f4e45108f4aa374812618e85e8d0f8dd91c0a
AnthoninHelias <heliasantho@yahoo.fr>
2022-11-29 10:46:34 +0100

    push

--- DevoirGraphiques - Etudiants/src/Controlers/CtrlGraphique.java
+++ DevoirGraphiques - Etudiants/src/Controlers/CtrlGraphique.java
@@ -21,6 +21,68 @@ public class CtrlGraphique
     public CtrlGraphique() {
         cnx = ConnexionBDD.getCnx();
     }
+    public HashMap<Integer,Float> GetDatasGraphique1()
+    {
+        HashMap<Integer, Float> datas = new HashMap();
+        try {
+            cnx = ConnexionBDD.getCnx();
+            ps = cnx.prepareStatement(
+                    "SELECT employe.ageEmp,AVG(employe.salaireEmp)\n"+
+                    "FROM employe\n"+
+                     "GROUP BY employe.ageEmp"
+            );
+            rs = ps.executeQuery();
+            while(rs.next())
+            {
+                datas.put(rs.getInt(1), rs.getFloat(2));
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CtrlGraphique.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return datas;
+    }
+    public HashMap<Integer,Integer> GetDatasGraphique2()
+    {
+        HashMap<Integer, Integer> datas = new HashMap();
+        try {
+            cnx = ConnexionBDD.getCnx();
+            ps = cnx.prepareStatement(
+                    "SELECT pourcentagehomme2.pourcentage , pourcentagefemme2.pourcentage\n"+
+                    "FROM pourcentagehomme2,pourcentagefemme2"
+
+            );
+            rs = ps.executeQuery();
+            while(rs.next())
+            {
+                datas.put(rs.getInt(1), rs.getInt(2));
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CtrlGraphique.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return datas;
+    }
+    public HashMap<Integer,String[]> GetDatasGraphique4()
+    {
+        HashMap<Integer,String[]> datas = new HashMap();
+        try {
+            cnx = ConnexionBDD.getCnx();
+            ps = cnx.prepareStatement("SELECT vente.montant , vente.nomMagasin , vente.nomSemestre\n" +
+                    "from vente");
+            rs = ps.executeQuery();
+            int i = 1;
+            while(rs.next())
+            {
+                datas.put(i, new String[]{rs.getString(1),rs.getString(2),rs.getString(3)});
+                i++;
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CtrlGraphique.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return datas;
+    }
 
 
 }
--- DevoirGraphiques - Etudiants/src/Vues/FrmGraphique.java
+++ DevoirGraphiques - Etudiants/src/Vues/FrmGraphique.java
@@ -16,12 +16,13 @@ import org.jfree.chart.plot.RingPlot;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultKeyedValues2DDataset;
 import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYZDataset;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
+import java.awt.event.*;
 import java.beans.PropertyChangeListener;
 import java.sql.SQLException;
 import java.util.Map;
@@ -46,5 +47,87 @@ public class FrmGraphique extends JFrame{
 
 
 
+        pnlGraph2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                DefaultPieDataset donnees = new DefaultPieDataset();
+
+                for (Map.Entry valeur : ctrlGraphique.GetDatasGraphique2().entrySet())
+                {
+                    donnees.setValue(valeur.getKey().toString(), Integer.parseInt(valeur.getValue().toString()));
+                }
+
+                JFreeChart chart1 = ChartFactory.createPieChart(
+                        "Pourcentage d'homme et femme",
+                        donnees,
+                        true, //
+                        true, //
+                        true //
+                );
+                ChartFrame frame = new ChartFrame("Graphique n°2", chart1);
+                frame.pack();
+                frame.setVisible(true);
+
+            }
+        });
+        pnlGraph4.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                // HISTOGRAMME
+                DefaultCategoryDataset donnees = new DefaultCategoryDataset();
+
+                for (Map.Entry valeur : ctrlGraphique.GetDatasGraphique4().entrySet())
+                {
+
+                }
+
+                JFreeChart chart1 = ChartFactory.createBarChart(
+                        "Montant des ventes  par magasin",
+                        "Magasins",
+                        "Montant des ventes",
+                        donnees,
+                        PlotOrientation.VERTICAL,
+                        true, true, false);
+                ChartFrame frame = new ChartFrame("Graphique n°4", chart1);
+                frame.pack();
+                frame.setVisible(true);
+
+            }
+        });
+        pnlGraph3.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+
+            }
+        });
+
+        /*
+        pnlGraph1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                DefaultCategoryDataset donnees = new DefaultCategoryDataset();
+
+                for (Map.Entry valeur : ctrlGraphique.GetDatasGraphique1().entrySet())
+                {
+                    donnees.setValue(valeur.getKey().toString()) , Float.parseFloat(valeur.getValue().toString()));
+                }
+                JFreeChart chart1 = ChartFactory.createLineChart("Salaire moyen " , "age des employes", "", donnees);
+                ChartFrame fra = new ChartFrame("Moyenne des salaires par age", chart1);
+                fra.pack();
+                fra.setVisible(true);
+            }
+        });
+        */
+
+        pnlGraph2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                super.mouseWheelMoved(e);
+            }
+        });
     }
 }
